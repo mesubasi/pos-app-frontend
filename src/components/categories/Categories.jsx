e324669098015902ba97cb487264a922f8b03263
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-05-21 20:03:40 +0300

    Front end API and CSS edits

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,35 +1,50 @@
 //Categories.jsx
 
-const Categories = () => {
+import { PlusOutlined } from "@ant-design/icons"
+import { useState } from "react";
+import { Button, Form, Input, Modal, message } from 'antd';
+import "./style.css"
+
+
+const Categories = ({ categories, setCategories }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [form] = Form.useForm()
+
+    const onFinish = (values) => {
+        try {
+            fetch("http://localhost:5000/api/categories/add-category", {
+                method: "POST", body: JSON.stringify(values), headers: { "Content-type": "application/json; charset=UTF-8" }
+            })
+            message.success("Category Succesfully Added.");
+            form.resetFields()
+            setCategories([...categories, values])
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <ul className="flex md:flex-col gap-4 text-lg">
-            <li className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>All</span>
+            {categories.map((item) => (
+                <li key={item._id} className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
+                    <span>{item.title}</span>
+                </li>
+            ))}
+            <li
+                className="category-item !bg-purple-800 hover:opacity-90"
+                onClick={() => setIsModalOpen(true)}>
+                <PlusOutlined className="md:text-2xl" />
             </li>
-            <li className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>Food</span>
-            </li>
-            <li className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>Drink</span>
-            </li>
-            <li className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>Fruit</span>
-            </li>
-            <li className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>Vegetable</span>
-            </li>
-            <li className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>Outfit</span>
-            </li>
-            <li className="bg-pink-600 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>+</span>
-            </li>
-            <li className="bg-pink-600 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>-</span>
-            </li>
-            <li className="bg-green-700 text-white hover:bg-pink-700 transition-all px-6 py-10 cursor-pointer text-center min-w-[145px]">
-                <span>Outfit</span>
-            </li>
+            <Modal title="Add New Category" open={isModalOpen} onCancel={() => setIsModalOpen(false)} footer={false}>
+                <Form layout="vertical" onFinish={onFinish} form={form}>
+                    <Form.Item name="title" label="Add Category" rules={[{ required: true, message: "Category Field Cannot Be Empty!", }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item className="flex justify-end mb-0">
+                        <Button type="primary" htmlType="submit" >Create</Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
         </ul>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,33 @@
+//HomePage.jsx
+
+import { useState, useEffect } from "react";
 import CartTotals from "../components/cart/CartTotals";
 import Categories from "../components/categories/Categories";
 import Header from "../components/header/Header";
 import Products from "../components/products/Products";
 
 const HomePage = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const getCategories = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/categories/get-all-category");
+                const data = await res.json();
+                setCategories(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getCategories();
+    }, []);
+
     return (
         <>
             <Header />
             <div className="home flex md:flex-row flex-col px-6 justify-between gap-10 md:pb-0 pb-20">
                 <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] -mr-3 pb-4">
-                    <Categories />
+                    <Categories categories={categories} setCategories={setCategories} />
                 </div>
                 <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-5">
                     <Products />
