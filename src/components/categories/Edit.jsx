318c1dbce47446267ab27602697e9c676d62b1d6
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-05-28 23:15:22 +0300

    Category Deletion Process Added

--- src/components/categories/Edit.jsx
+++ src/components/categories/Edit.jsx
@@ -3,25 +3,43 @@ import React, { useState } from 'react'
 
 export const Edit = ({ isModalEditOpen, setIsModalEditOpen, categories, setCategories }) => {
     const [editingRow, setEditingRow] = useState({})
-    const onFinish = (values) => {
+    const onFinish = async (values) => {
         try {
-            fetch("http://localhost:5000/api/categories/update-category",
+            await fetch("http://localhost:5000/api/categories/update-category",
                 {
                     method: "PUT",
                     body: JSON.stringify({ ...values, categoryId: editingRow._id }),
                     headers: { "Content-type": "application/json; charset=UTF-8" },
                 })
-            message.success("Kategori Başarıyla Güncellendi!")
+            message.success("Category Successfully Updated!")
             setCategories(categories.map((item) => {
                 if (item._id === editingRow._id) {
-                    return { ...item, title: values.title }
+                    return { ...record, title: values.title }
                 } else {
-                    return item
+                    return record
                 }
             }))
         } catch (err) {
-            message.success("Bir Şeyler Yanlış Gitti!")
+            message.error("Something went wrong!")
             console.log(err);
+        }
+    }
+
+    const deleteCategory = async (id) => {
+        if (window.confirm("Are you sure?")) {
+            try {
+                await fetch("http://localhost:5000/api/categories/delete-category", {
+                    method: "DELETE",
+                    body: JSON.stringify({ categoryId: id }),
+                    headers: { "Content-type": "application/json; charset=UTF-8" },
+                })
+                message.success("Category Deleted Successfully")
+                setCategories(categories.filter((item) => item._id !== id))
+            } catch (err) {
+                message.error("Something went wrong!")
+                console.log(err);
+            }
+
         }
     }
 
@@ -29,27 +47,27 @@ export const Edit = ({ isModalEditOpen, setIsModalEditOpen, categories, setCateg
         {
             title: "Category Title",
             dataIndex: "title",
-            render: (_, item) => {
-                if (item._id === editingRow._id) {
+            render: (_, record) => {
+                if (record._id === editingRow._id) {
                     return (
-                        <Form.Item className='mb-0' name="title">
-                            <Input defaultValue={item.title} />
-                        </Form.Item>
+                        <Form.record className='mb-0' name="title">
+                            <Input defaultValue={record.title} />
+                        </Form.record>
                     )
                 } else {
-                    return <p>{item.title}</p>
+                    return <p>{record.title}</p>
                 }
             }
         },
         {
             title: "Action",
             dataIndex: "Action",
-            render: (text, item) => {
+            render: (_, record) => {
                 return (
                     <div>
-                        <Button type="link" onClick={() => setEditingRow(item)} className="pl-0">Edit</Button>
-                        <Button type="link" htmlType="Submit" className="text-gray-500" >Save</Button>
-                        <Button type="link" danger>Delete</Button>
+                        <Button type="link" onClick={() => setEditingRow(record)} className="pl-0">Edit</Button>
+                        <Button type="link" htmlType="Submit" className="text-gray-500">Save</Button>
+                        <Button type="link" danger onClick={() => deleteCategory(record._id)}>Delete</Button>
                     </div>
                 )
             }
@@ -57,7 +75,7 @@ export const Edit = ({ isModalEditOpen, setIsModalEditOpen, categories, setCateg
     ]
 
     return (
-        <Modal open={isModalEditOpen} onCancel={() => setIsModalEditOpen = (false)} title="Category Operations" footer={false}>
+        <Modal open={isModalEditOpen} onCancel={() => setIsModalEditOpen(false)} title="Category Operations" footer={false}>
             <Form onFinish={onFinish}>
                 <Table bordered dataSource={categories} columns={columns} rowKey={"_id"} />
             </Form>
