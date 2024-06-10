1705f031d7fcb0c111df7903f1aabb202c3738d1
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-10 23:26:38 +0300

    Products Separated into Components

---
+++ src/components/products/ProductItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProductItem = ({ item }) => {
+    return (
+        <div className="product-item hover:shadow-lg transition-all border cursor-pointer select-none">
+            <div className="product-img">
+                <img src={item.img} alt="" className="h-28 object-cover w-full border-b" />
+            </div>
+            <div className="product-info flex flex-col p-3">
+                <span className="font-bold">{item.title}</span>
+                <span>{item.price}&nbsp;$</span>
+            </div>
+        </div>
+    )
+}
+
+export default ProductItem
