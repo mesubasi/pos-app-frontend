ca765902da8308fe8037facaddd059d398bdbb2b
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-18 10:38:59 +0300

    Added control to increase the quantity value of the same products

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -9,7 +9,15 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      state.cartItems.push(action.payload);
+      const findCartItem = state.cartItems.find(
+        (item) => item._id === action.payload._id
+      );
+
+      if (findCartItem) {
+        findCartItem.quantity++;
+      } else {
+        state.cartItems.push(action.payload);
+      }
     },
   },
 });
