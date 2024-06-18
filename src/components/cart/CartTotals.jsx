3aa878cf9015edefc8f7a3a2997f518e4f7b1918
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-18 10:31:10 +0300

    Mapping for cartItems to be added to cart

--- src/components/cart/CartTotals.jsx
+++ src/components/cart/CartTotals.jsx
@@ -11,20 +11,22 @@ const CartTotals = () => {
     <div className="cart h-full flex flex-col max-h-[calc(100vh_-_90px)]">
       <h2 className="text-white bg-blue-600 text-center py-4 font-bold tracking-wide">Products in Cart</h2>
       <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
-        <li className="cart-item flex justify-between">
-          <div className="flex items-center gap-x-">
-            <img src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" className="w-16 h-16 object-cover" />
-            <div className="flex flex-col ml-2">
-              <b>Elma</b>
-              <span>12$ x 2</span>
+        {cart.cartItems.map((item) => (
+          <li className="cart-item flex justify-between" key={item._id}>
+            <div className="flex items-center gap-x-">
+              <img src={item.img} alt="" className="w-16 h-16 object-cover" />
+              <div className="flex flex-col ml-2">
+                <b>{item.title}</b>
+                <span>{item.price}$ x 2</span>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center">
-            <Button type="primary" size="small" className="w-full flex items-center justify-center rounded-full" icon={<PlusCircleOutlined />} />
-            <span className="px-1 font-bold">1</span>
-            <Button type="primary" size="small" className="w-full flex items-center justify-center rounded-full" icon={<MinusCircleOutlined />} />
-          </div>
-        </li>
+            <div className="flex items-center">
+              <Button type="primary" size="small" className="w-full flex items-center justify-center rounded-full" icon={<PlusCircleOutlined />} />
+              <span className="px-1 font-bold">1</span>
+              <Button type="primary" size="small" className="w-full flex items-center justify-center rounded-full" icon={<MinusCircleOutlined />} />
+            </div>
+          </li>
+        ))}
       </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
