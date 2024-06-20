f01916793cf585228d27568ddfd30abb71f9d864
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-20 10:19:55 +0300

    State changed

--- src/components/cart/CartTotals.jsx
+++ src/components/cart/CartTotals.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { deleteCart, addProduct } from "../../redux/cartSlice";
 
 const CartTotals = () => {
-  const { cartItems } = useSelector((state) => state.cart);
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const deleteClick = (item) => {
@@ -23,7 +23,7 @@ const CartTotals = () => {
     <div className="cart h-full flex flex-col max-h-[calc(100vh_-_90px)]">
       <h2 className="text-white bg-blue-600 text-center py-4 font-bold tracking-wide">Products in Cart</h2>
       <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
-        {cartItems.map((item) => (
+        {cart.map((item) => (
           <li className="cart-item flex justify-between" key={item._id}>
             <div className="flex items-center gap-x-">
               <img src={item.img} alt="" className="w-16 h-16 object-cover cursor-pointer" onClick={() => deleteClick(item)} />
