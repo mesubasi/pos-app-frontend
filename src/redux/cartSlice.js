7f621165a1f733eca1ae15faa56b23bbe86852c8
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-21 22:01:53 +0300

    ,Added for cart clear state

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -49,8 +49,13 @@ const cartSlice = createSlice({
         );
       }
     },
+    reset: (state) => {
+      state.cartItems = [];
+      state.total = 0;
+    },
   },
 });
 
-export const { addProduct, deleteCart, increase, decrease } = cartSlice.actions;
+export const { addProduct, deleteCart, increase, decrease, reset } =
+  cartSlice.actions;
 export default cartSlice.reducer;
