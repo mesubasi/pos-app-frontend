4c59dd63693154076825b7990130f430811c2f27
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-01 22:12:19 +0300

    Cart Reversed

--- src/components/cart/CartTotals.jsx
+++ src/components/cart/CartTotals.jsx
@@ -53,7 +53,7 @@ const CartTotals = () => {
               <Button type="primary" size="small" className="w-full flex items-center justify-center rounded-full" onClick={() => decreaseClick(item)} icon={<MinusCircleOutlined />} />
             </div>
           </li>
-        )) : <span className="text-center md:animate-bounce">Empty Cart</span>}
+        )).reverse() : <span className="text-center md:animate-bounce">Empty Cart</span>}
       </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
