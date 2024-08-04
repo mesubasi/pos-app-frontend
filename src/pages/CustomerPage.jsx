32aeb2ef55adf5947fcb536686f7d830d8dbb696
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-04 22:14:47 +0300

    dataSource deleted

--- src/pages/CustomerPage.jsx
+++ src/pages/CustomerPage.jsx
@@ -21,21 +21,6 @@ const CustomerPage = () => {
         getInvoices();
     }, []);
 
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
-
     const columns = [
         {
             title: 'Name',
