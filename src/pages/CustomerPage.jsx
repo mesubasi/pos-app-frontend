d150b1bcab527d0a86dbe048839eabd34f1cc7aa
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-04 22:22:40 +0300

    Scroll Added

--- src/pages/CustomerPage.jsx
+++ src/pages/CustomerPage.jsx
@@ -47,7 +47,7 @@ const CustomerPage = () => {
             <Header />
             <div className="px-6">
                 <h2 className='text-4xl font-bold text-center mb-4'>Customers</h2>
-                <Table dataSource={invoiceItems} columns={columns} bordered pagination={false} />
+                <Table dataSource={invoiceItems} columns={columns} bordered pagination={false} scroll={{ x: 1000, y: 300 }} />
                 <div className="cart-total flex justify-end mt-4">
                     <Card className="w-72 border-2">
                     </Card>
