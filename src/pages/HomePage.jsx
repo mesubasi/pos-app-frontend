beb50ca9364a69ffe5e65294e5f4b6f4d55c6c3d
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-12 23:50:31 +0300

    Move Props

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,20 @@ import Products from "../components/products/Products";
 const HomePage = () => {
     const [categories, setCategories] = useState([]);
     const [filtered, setFiltered] = useState([]);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/products/get-all-product");
+                const data = await res.json();
+                setProducts(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getProducts();
+    }, []);
 
     useEffect(() => {
         const getCategories = async () => {
@@ -30,10 +44,10 @@ const HomePage = () => {
             <Header />
             <div className="home flex md:flex-row flex-col px-6 justify-between gap-10 md:pb-0 pb-20 h-screen">
                 <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] -mr-3 pb-4">
-                    <Categories categories={categories} setCategories={setCategories} setFiltered={setFiltered} />
+                    <Categories categories={categories} setCategories={setCategories} setFiltered={setFiltered} products={products} />
                 </div>
                 <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-5 min-h-[500px]">
-                    <Products categories={categories} filtered={filtered} />
+                    <Products categories={categories} filtered={filtered} products={products} setProducts={setProducts} />
                 </div>
                 <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
                     <CartTotals />
