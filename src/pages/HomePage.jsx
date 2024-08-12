fb89132038975f2d56068a26d105bf0281b67b76
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-12 23:33:44 +0300

    Scroll Fixed For Mobile

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -31,7 +31,7 @@ const HomePage = () => {
                 <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] -mr-3 pb-4">
                     <Categories categories={categories} setCategories={setCategories} />
                 </div>
-                <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-5">
+                <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-5 min-h-[500px]:">
                     <Products categories={categories} />
                 </div>
                 <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
