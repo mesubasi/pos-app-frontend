acb89bbbe752c5c3862e98288d997ad15cf3310f
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-26 21:46:15 +0300

    Localstorage used for cart

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -3,7 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cartItems: [],
+    cartItems: localStorage.getItem("cart")
+      ? localStorage.getItem("cart").cartItems
+      : [],
     total: 0,
     tax: 8,
   },
